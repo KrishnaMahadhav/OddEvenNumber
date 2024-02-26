5ecfb03237f9dcb2f22d461d2ffc8972c8d53877
Krishnakumar Sankar <98452642+KrishnaMahadhav@users.noreply.github.com>
2024-02-26 17:56:20 +0530

    Add files via upload

---
+++ odd.java
@@ -0,0 +1,22 @@
+package oddeven;
+
+public class odd {
+	
+	
+	    public static void main(String[] args)
+	    {
+	int x = 100;
+	if (x%2 == 0)
+
+	System.out.println("Given number is Even");
+
+	else
+
+	System.out.println("Given number is Odd");
+
+
+	    }
+	        
+	    }
+
+
